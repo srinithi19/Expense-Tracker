332b0db138824bc8f873d587a0bc8caf3aef9522
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-10 10:40:05 -0800

    rener homepage

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -2,6 +2,21 @@ const router = require('express').Router();
 const { User,Transaction,Budget } = require('../models');
 const withAuth = require('../utils/auth');
 
+
+//Rrender application homepage
+router.get('/', async (req, res) => {
+    console.log("-------------------")
+    console.log("USER1" + req.session.username);
+    console.log("-------------------")
+
+    res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn,
+        user: req.session.user_id,
+        name: req.session.name,
+    });
+  });
+
 //check to see if user is loggedin or else display login/signup form
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
