779c975ae4afebf26599a9daa0cc82e6b5d09f41
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-11 12:34:57 -0800

    fixed button sytling

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -28,8 +28,6 @@ router.get('/signup', (req, res) => {
 
 
 //HTML page to render users transactions
-//TODO - add withAuth
-
 router.get('/profile', withAuth, (req, res) => {
   if (!req.session.loggedIn) {
     res.redirect('/');
