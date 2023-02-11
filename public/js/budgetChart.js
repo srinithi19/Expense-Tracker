056df98f17f1241308f25d15ab3d94d7b87fff3c
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-11 15:04:46 -0800

    fix chart values

--- public/js/budgetChart.js
+++ public/js/budgetChart.js
@@ -1,12 +1,13 @@
-
 var ctx = document.getElementById("chart").getContext('2d');
+var income = document.getElementById("sumIncome");
+var expense = document.getElementById("sumExpense");
 
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
         labels: [ "Expense","Income"],
         datasets: [{    
-            data: [500,1404], // Specify the data values array
+            data: [expense,income], // Specify the data values array
          
             borderColor: ['#f443368c','#00968896'], // Add custom color border
             backgroundColor: ['#f443368c', '#00968896'], // Add custom color background (Points and Fill)
