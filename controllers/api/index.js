908c5bcd52f21dedc7247b504aaa4d135aed84d2
Becka McNally <beckamcnally@gmail.com>
2023-02-12 16:12:41 -0800

    challenge client side js implementation

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const transRoutes = require('./transactionRoutes');
 const budgetRoutes = require('./budgetRoutes');
+const challengeRoutes = require('./challengeRoutes');
 
 router.use('/users', userRoutes);
 router.use('/transaction', transRoutes);
 router.use('/budget', budgetRoutes);
+router.use('/challenge', challengeRoutes);
 
 module.exports = router;
--- public/js/addChallenge.js
+++ public/js/addChallenge.js
@@ -5,5 +5,30 @@ const replaceForm = () => {
     newChallengeEl.style.display = "inline";
 };
 
+const submitFromHandler = async (event) => {
+  event.preventDefault();
+  const content = document.getElementById('challengeText').value;
+
+  if(content){
+    const response = await fetch('/api/challenge/addChallenge', {
+        method: "POST",
+        body: JSON.stringify({content}),
+        headers: { "Content-Type": "application/json" }
+    });
+    console.log(response)
+    if(response.ok){
+      document.location.replace("/challenges");
+    } else {
+
+      alert("Failed to add Challenge.")
+    }
+  } else {
+    let messageContainer = getElementById('#message-container')
+      let message = document.createElement("div")
+      message.innerHTML = 'Please enter a Challenge';
+      messageContainer.append(message)
+  }
+}
 
 setChallengeBtn.addEventListener('click', replaceForm);
+newChallengeEl.addEventListener('click', submitFromHandler);
