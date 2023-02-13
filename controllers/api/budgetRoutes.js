8ca0798b8a5331a571ec8b7164a6d485c76625d5
DBENSAN <dftb97@gmail.com>
2023-02-13 05:17:25 -0800

    Adds fixes to budget routes

--- controllers/api/budgetRoutes.js
+++ controllers/api/budgetRoutes.js
@@ -22,4 +22,29 @@ router.put("/update", withAuth, async (req, res) => {
   }
 });
 
+router.post("/add", withAuth, async (req, res) => {
+  try {
+    const existingBudget = await Budget.findOne({
+      where: {
+        user_id: req.session.user_id
+      }
+    });
+
+    if (existingBudget) {
+      res.status(400).json({ message: "Budget already exists" });
+      return;
+    }
+
+    const newBudget = await Budget.create({
+      user_id: req.session.user_id,
+      amount: req.body.amount
+    });
+
+    res.status(200).json({ newBudget });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
